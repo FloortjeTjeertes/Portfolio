8116ad551006e4dd183767e976d3147e00b348e3
florianmac <nairolf3>
2019-10-11 14:14:56 +0200

    sdrghdhdfdgf

--- private/views/boddyMain.php
+++ private/views/boddyMain.php
@@ -1,20 +1,10 @@
 <link rel="stylesheet" type="text/css" href="css/main.css" style="display:hidden;">
-<div class="pictureSlide"><p class="pictureSlide__text">hey welkom je hebt mijn website gevonden!! nu kan je zien hoe geweldig ik ben (of niet). </p></div>
-
-<div class="textBox textBox__container--een"> <div class="textBox__white"><div  class="textBox__triangle textBox__triangle--pink"></div> <p>hey welko op mijn website ik ben florian ik zit op media college amsterdam en doe de opleiding media development</p> </div> <div class="textBox__triangle textBox__triangle--blue"></div></div>
-<div class=gameboys>
-<div class="gameboy gameboy__een"> <div class="gameboy__main gameboy__main--big"> <div class="gameboy__main gameboy__laybel">plaatje naam 1</div> </div> <div class="gameboy__main gameboy__main--small"></div> </div>
-<div class="gameboy gameboy__twee"> <div class="gameboy__main gameboy__main--big"> <div class="gameboy__main gameboy__laybel">plaatje naam 1</div> </div> <div class="gameboy__main gameboy__main--small"></div> </div>
+<div class="sidebar__folders">
+ <div class="sidebar__folder sidebar__folders--een"> <div class="sidebar__folder--oranje1"></div><div class="sidebar__folder--oranje2"></div><div class=sidebar__folder--zwart></div>  </div>
+ <div class="sidebar__folder sidebar__folders--twee"> <div class="sidebar__folder--oranje1"></div><div class="sidebar__folder--oranje2"></div><div class=sidebar__folder--zwart></div> </div>
+ <div class="sidebar__folder sidebar__folders--drie"> <div class="sidebar__folder--oranje1"></div><div class="sidebar__folder--oranje2"></div><div class=sidebar__folder--zwart></div> </div>
+ <div class="sidebar__folder sidebar__folders--vier"> <div class="sidebar__folder--oranje1"></div><div class="sidebar__folder--oranje2"></div><div class=sidebar__folder--zwart></div> </div>
 </div>
 
 
-<div class="textBox textBox__container--twee"> <div class="textBox__white"><div class="textBox__triangle textBox__triangle--pink"></div><p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.</p></div>
-<div class="textBox__triangle textBox__triangle--blue"></div></div>
-<div class="gameboys__twee">
-<div class="gameboy gameboy__drie"> <div class="gameboy__main gameboy__main--big"> <div class="gameboy__main gameboy__laybel">project the wall</div> <img  class="gameboy__gameboy-drie--plaatje" src="pictures/the wall.png"> </div> <div class="gameboy__main gameboy__main--small"></div> </div>
-<div class="gameboy gameboy__vier"> <div class="gameboy__main gameboy__main--big"> <div class="gameboy__main gameboy__laybel">nes game        </div> <img  class="gameboy__gameboy-vier--plaatje"> </div>                             <div class="gameboy__main gameboy__main--small"></div> </div>
-<div class="gameboy gameboy__vijf"> <div class="gameboy__main gameboy__main--big"> <div class="gameboy__main gameboy__laybel">plaatje naam 1  </div> </div> <div class="gameboy__main gameboy__main--small"></div> </div>
-<div class="gameboy gameboy__zes">  <div class="gameboy__main gameboy__main--big"> <div class="gameboy__main gameboy__laybel">plaatje naam 1  </div> </div> <div class="gameboy__main gameboy__main--small"></div> </div>
-
-</div>
 <a href="/wall">Visit W3Schools</a>
