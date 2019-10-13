9485b21fee0f38b298aa2c8f65992d11de60248e
florianmac <nairolf3>
2019-10-13 13:31:01 +0200

    fsdf

--- private/views/boddyMain.php
+++ private/views/boddyMain.php
@@ -10,5 +10,5 @@
 
 <div class="window">
   <div class="window__header--oranje"> <div class="window__header--zwart"></div></div>
-<div class="window__textfield"></div>
+<div class="window__textfield">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus gravida quam et nisi viverra, et pellentesque dolor maximus. Integer malesuada libero eu enim tincidunt faucibus. Vestibulum nibh diam, pulvinar vitae porttitor ut, blandit non mi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec ullamcorper lobortis leo, sit amet lobortis odio faucibus ac. Quisque metus odio, imperdiet sit amet arcu pulvinar, condimentum placerat sem. Nam at dui euismod, mattis nulla eu, accumsan libero. Donec mauris lectus, gravida porta imperdiet a, scelerisque eget purus.</div>
 </div>
