aa33f06d57ac346cd790ed4af2d0160651c7d6ed
florianmac <nairolf3>
2019-10-13 12:48:14 +0200

    sdfsf

--- private/views/boddyMain.php
+++ private/views/boddyMain.php
@@ -1,7 +1,13 @@
 <link rel="stylesheet" type="text/css" href="css/main.css" style="display:hidden;">
+
 <div class="sidebar__folders">
  <div class="sidebar__folder sidebar__folders--een"> <div class="sidebar__folder--oranje1"></div><div class="sidebar__folder--oranje2"></div><div class=sidebar__folder--zwart></div>  </div>
  <div class="sidebar__folder sidebar__folders--twee"> <div class="sidebar__folder--oranje1"></div><div class="sidebar__folder--oranje2"></div><div class=sidebar__folder--zwart></div> </div>
  <div class="sidebar__folder sidebar__folders--drie"> <div class="sidebar__folder--oranje1"></div><div class="sidebar__folder--oranje2"></div><div class=sidebar__folder--zwart></div> </div>
  <div class="sidebar__folder sidebar__folders--vier"> <div class="sidebar__folder--oranje1"></div><div class="sidebar__folder--oranje2"></div><div class=sidebar__folder--zwart></div> </div>
+</div>
+<img src="pictures/placeHolder.jpg" alt='placeholder'>
+<div class="window">
+  <div class="window__header--oranje"></div><div class="window__header--zwart"></div>
+<div class="window__textfield"></div>
 </div>
