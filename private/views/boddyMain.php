b42ba2fb7275e15d1e9c1b139ce0729868c6a88f
florianmac <nairolf3>
2019-10-23 16:03:01 +0200

    eteterte

--- private/views/boddyMain.php
+++ private/views/boddyMain.php
@@ -5,10 +5,17 @@
  <div class="sidebar__folder sidebar__folders--twee"> <div class="sidebar__folder--oranje1"></div><div class="sidebar__folder--oranje2"></div><div class=sidebar__folder--zwart></div> </div>
  <div class="sidebar__folder sidebar__folders--drie"> <div class="sidebar__folder--oranje1"></div><div class="sidebar__folder--oranje2"></div><div class=sidebar__folder--zwart></div> </div>
  <div class="sidebar__folder sidebar__folders--vier"> <div class="sidebar__folder--oranje1"></div><div class="sidebar__folder--oranje2"></div><div class=sidebar__folder--zwart></div> </div>
+ <div class="sidebar__folder sidebar__folders--github"><a href="https://github.com/floriantjeertes" target="_blank"><img src="pictures/kitty.png" alt="github-logo"></a> å </div>
+ <div class="sidebar__folder sidebar__folders--linkedIn"><a href="https://www.linkedin.com/in/florian-tjeertes-b32497183" target="_blank"><img src="pictures/1200px-Linkedin.svg.png" alt="github-logo"></a></div>
+
 </div>
 <img class="window__img" src="pictures/placeHolder.jpg" alt='placeholder'>
 
 <div class="window">
   <div class="window__header--oranje"> <div class="window__header--zwart"></div></div>
-<div class="window__textfield">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus gravida quam et nisi viverra, et pellentesque dolor maximus. Integer malesuada libero eu enim tincidunt faucibus. Vestibulum nibh diam, pulvinar vitae porttitor ut, blandit non mi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec ullamcorper lobortis leo, sit amet lobortis odio faucibus ac. Quisque metus odio, imperdiet sit amet arcu pulvinar, condimentum placerat sem. Nam at dui euismod, mattis nulla eu, accumsan libero. Donec mauris lectus, gravida porta imperdiet a, scelerisque eget purus.</div>
+<div class="window__textfield">welkom op mijn website ik ben florian tjeertes en dit is mijn portfolio!  </div>
+</div>
+<div class="window window_-move">
+  <div class="window__header--oranje"> <div class="window__header--zwart"></div></div>
+<div class="window__textfield">test</div>
 </div>
