1d6c9e2a683c64d828a47e6a8fd9a195fe4c69df
florianmac <nairolf3>
2019-11-05 11:18:47 +0100

    mnb , b
    hgvgvghvhghkhvhv

--- private/views/projecten.php
+++ private/views/projecten.php
@@ -1,7 +1,11 @@
-
-<div class="folder slider">
-<div class="project1"></div>
-<div class="project2"></div>
-<div class="project3"></div>
-<div class="project4"></div>
+<link rel="stylesheet" href="css/project.css">
+<div class="folder__slider">
+  <div class="window__header--oranje"> <div class="window__header--zwart"></div></div>
+  <div class="arrow__left"></div>
+<div class="project one"><img class="the_wall" src="pictures/the wall.png"></div>
+<div class="project two"><img class="the_wall" src="pictures/the wall.png"></div>
+<div class="project three"><img class="the_wall" src="pictures/the wall.png"></div>
+<div class="project four"><img class="the_wall" src="pictures/the wall.png"></div>
+<div class="arrow__right"></div>
 </div>
+<script src="./javascript/folder.js"></script>
