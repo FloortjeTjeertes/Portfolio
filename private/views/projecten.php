c221403bf97f011b7e924eb674a86a33b4f26ccd
florianmac <nairolf3>
2019-11-05 20:25:23 +0100

    adasa

--- private/views/projecten.php
+++ private/views/projecten.php
@@ -2,10 +2,10 @@
 <div class="folder__slider" id="slider">
   <div class="window__header--oranje"> <div class="window__header--zwart"></div></div>
 <div class="folder__slider--grid">
-<div id="1" class="project one" onclick="modaal(1)"><img class="the_wall" src="pictures/the wall.png"></div>
-<div class="project two"><a hreff="listener"><img class="the_wall" src="pictures/nes rom.png"></a></div>
-<div class="project three"><a hreff=""><img class="the_wall" src="pictures/the wall.png"></a></div>
-<div class="project four"><a hreff=""><img class="the_wall" src="pictures/the wall.png"></a></div>
+<div id="1" class="project one"><a hreff=""><img class="the_wall" src="pictures/the wall.png"></a></div>
+<div class="project two"><a hreff="listener.php?modaal=2"><img class="the_wall" src="pictures/nes rom.png"></a></div>
+<div class="project three"><a hreff="listener.php?modaal=3"><img class="the_wall" src="pictures/the wall.png"></a></div>
+<div class="project four"><a hreff="listener.php?modaal=4"><img class="the_wall" src="pictures/the wall.png"></a></div>
 </div>
 
 </div>
